91350f2781409c436d6a02c50ea1f48f4c3862a5
foreverinban <104363549+foreverinban@users.noreply.github.com>
2022-04-25 17:20:12 +0500

    Add files via upload

---
+++ (Transport_task) main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+
+	int tar[3][4];
+	int zap[4];
+	int ptr[3];
+	int i, j, z;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			tar[i][j] = rand() % 10;
+		}
+	}
+	// генерайция тарифов
+	for (i = 0; i < 4; i++)
+	{
+		cout << "Вводите запасы" << endl;;
+		cin >> zap[i];
+	}
+	// ввод запасов
+	for (i = 0; i < 3; i++)
+	{
+		cout << "Введите потребности" << endl;;
+		cin >> ptr[i];
+	}
+	// ввод потребностей
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			cout<<tar[i][j]<<' ';
+		}
+		cout << endl;
+	}
+
+	//северо-западный угол
+	cout <<endl<< "Базовое решение" << endl;
+	
+}
